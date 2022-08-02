ced3a4a01bd6790090a464d7180c98e92f10baf2
Anush A <anushsaint03@gmail.com>
2022-08-02 10:53:47 +0530

    updated

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+import {availibility, database, fetchMovieAvailability, fetchMovieList} from "./api.js"
+const loader= document.getElementById('loader');
+
+window.onload=function async (){
+    fetchMovieList().then(res=>{
+        res.forEach(movie=>{
+            const moviecontainer= document.querySelector(".movie-container")
+            const anchor= document.createElement("a");
+            anchor.classList.add("movie-link")
+            anchor.innerHTML=`
+                <div class="movie">
+                    <div class="movie-img-wrapper" style="
+                        background-image: url(${movie.imgUrl})">
+                    </div>
+                    <h4>${movie.name}</h4>
+                </div>
+            `
+            moviecontainer.appendChild(anchor);
+        })
+        document.getElementById('loader').remove();
+    }
+        );
+}
+
+document.querySelector("movie-link").addEventListener("click",()=>{
+    document.getElementById("booker").classList.toggle("show")
+})
+
